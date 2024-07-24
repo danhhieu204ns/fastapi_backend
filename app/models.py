6c52915299d39fee9250d0c1ebd7b7c0cff663f9
Danh Hieu <danhhieu123ns@gmail.com>
2024-07-24 20:36:48 +0700

    create user table

--- app/models.py
+++ app/models.py
@@ -1,9 +1,21 @@
 from .database import Base
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, text
 from sqlalchemy.sql.expression import null
+from sqlalchemy.sql.sqltypes import TIMESTAMP
+import time
 
 class Post(Base):
     __tablename__ = "posts"
+
     id = Column(Integer, primary_key=True, nullable=False)
     title = Column(String, nullable=False)
     content = Column(String, nullable=False)
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, nullable=False)
+    email = Column(String, nullable=False, unique=True)
+    password = Column(String, nullable=False)
+    create_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
