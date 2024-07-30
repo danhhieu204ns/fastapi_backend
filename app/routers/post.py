f8f386f27761dace840c54b6505f9c018fb8634b
Danh Hieu <danhhieu123ns@gmail.com>
2024-07-30 22:05:43 +0700

    get your own posts

--- app/routers/post.py
+++ app/routers/post.py
@@ -12,16 +12,21 @@ router = APIRouter(
 @router.get("/", response_model=List[schemas.PostResponse])
 async def getPosts(db: Session = Depends(get_db), 
                    current_user: int = Depends(oauth2.get_current_user)):
-    posts = db.query(models.Post).all()
+    
+    posts = db.query(models.Post).filter(models.Post.owner_id == current_user.id).all()
     return posts
 
 @router.get("/{id}", response_model=schemas.PostResponse)
 async def getPost(id: int, 
-                  db: Session = Depends(get_db)):
+                  db: Session = Depends(get_db),
+                  current_user: int = Depends(oauth2.get_current_user)):
     post = db.query(models.Post).filter(models.Post.id == id).first()
     if not post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f"Not found post with {id}!")
+    if post.owner_id != current_user.id:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
+                            detail=f"Not alowed post with {id}!")
     return post
 
 @router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.PostResponse)
