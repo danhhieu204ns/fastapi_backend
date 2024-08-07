4acd56a5b4761db7c43c0a22947496a774e0c267
Danh Hieu <danhhieu123ns@gmail.com>
2024-08-07 20:51:38 +0700

    add get group by id

--- app/routers/group.py
+++ app/routers/group.py
@@ -1,5 +1,5 @@
 from fastapi import status, HTTPException, APIRouter, Depends
-from .. import models, schemas, utils, database
+from .. import models, schemas, utils, database, oauth2
 from sqlalchemy.orm import Session
 from sqlalchemy import func
 from typing import List, Optional
@@ -19,3 +19,21 @@ async def getGroups(db: Session = Depends(database.get_db),
     groups = db.query(models.Group).limit(limit).offset(skip).all()
 
     return groups
+
+@router.get("/{id}", 
+            response_model=schemas.GroupResponse)
+async def getgroup(id: int, 
+                  db: Session = Depends(database.get_db),
+                  current_user = Depends(oauth2.get_current_user)):
+    
+    group = db.query(models.Group).filter(models.Group.id == id).first()
+
+    if not group:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"Not found group with {id}!")
+    
+    if group.admin_id != current_user.id:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
+                            detail=f"Not allowed group with {id}!")
+    
+    return group
