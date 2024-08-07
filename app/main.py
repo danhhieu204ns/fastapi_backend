4d2334edf48a4eeeec27fe195a6148d85e45e576
Danh Hieu <danhhieu123ns@gmail.com>
2024-08-07 16:27:35 +0700

    add group router to main.py

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from .routers import post, user, auth, vote
+from .routers import post, user, auth, vote, group
 from . import models
 from .database import engine
 
@@ -32,3 +32,4 @@ app.include_router(post.router)
 app.include_router(user.router)
 app.include_router(auth.router)
 app.include_router(vote.router)
+app.include_router(group.router)
