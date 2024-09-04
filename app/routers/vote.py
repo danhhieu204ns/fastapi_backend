7361583392f4e70d2d8cab3dc232fba103b1b9b2
Danh Hieu <danhhieu123ns@gmail.com>
2024-09-04 15:06:44 +0700

    repo votes

--- app/routers/vote.py
+++ app/routers/vote.py
@@ -1,6 +1,7 @@
 from fastapi import status, HTTPException, Depends, APIRouter
 from .. import schemas, database, models, oauth2
 from sqlalchemy.orm import Session
+from ..repository import vote
 
 
 router = APIRouter(
@@ -9,27 +10,11 @@ router = APIRouter(
 )
 
 @router.post("/", status_code=status.HTTP_201_CREATED)
-async def vote(vote: schemas.VoteCreate, 
-               db: Session = Depends(database.get_db), 
-               current_user: int = Depends(oauth2.get_current_user)):
+async def handle_vote(vote_info: schemas.VoteCreate, 
+                      db: Session = Depends(database.get_db), 
+                      current_user: int = Depends(oauth2.get_current_user)):
     
-    vote_query = db.query(models.Vote).filter(models.Vote.post_id == vote.post_id, 
-                                              models.Vote.user_id == current_user.id)
-    vote_found = vote_query.first()
+    return vote.handle_vote(vote_info, db, current_user)
 
-    if vote.dir == 1:
-        if vote_found:
-            vote_query.delete(synchronize_session=False)
-            db.commit()
-            return {"message": "Unliked"}
-        else:
-            new_vote = models.Vote(post_id = vote.post_id, user_id = current_user.id)
-            db.add(new_vote)
-            db.commit()
-            return {"message": "Liked"}
-    else:
-        if vote_found:
-            vote_query.delete(synchronize_session=False)
-            db.commit()
-            return {"message": "Unliked"}
+
         
