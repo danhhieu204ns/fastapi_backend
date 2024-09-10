f6c73b78986e13d5492a6ea74c2a762d09aeaf2d
Danh Hieu <danhhieu123ns@gmail.com>
2024-09-10 17:13:21 +0700

    update vote

--- app/repository/vote.py
+++ app/repository/vote.py
@@ -3,25 +3,29 @@ from sqlalchemy.orm import Session
 
 
 def handle_vote(vote: schemas.VoteCreate, 
-         db: Session, 
-         current_user):
+                db: Session, 
+                current_user):
     
     vote_query = db.query(models.Vote).filter(models.Vote.post_id == vote.post_id, 
                                               models.Vote.user_id == current_user.id)
     vote_found = vote_query.first()
+    
+    message = ""
 
     if vote.dir == 1:
         if vote_found:
             vote_query.delete(synchronize_session=False)
             db.commit()
-            return {"message": "Unliked"}
+            message = "Unliked"
         else:
             new_vote = models.Vote(post_id = vote.post_id, user_id = current_user.id)
             db.add(new_vote)
             db.commit()
-            return {"message": "Liked"}
+            message = "Liked"
     else:
         if vote_found:
             vote_query.delete(synchronize_session=False)
             db.commit()
-            return {"message": "Unliked"}
+            message = "Unliked"
+        
+    return {"message": message}
--- app/routers/vote.py
+++ app/routers/vote.py
@@ -1,5 +1,5 @@
-from fastapi import status, HTTPException, Depends, APIRouter
-from .. import schemas, database, models, oauth2
+from fastapi import status, Depends, APIRouter
+from .. import schemas, database, oauth2
 from sqlalchemy.orm import Session
 from ..repository import vote
 
